2dc2c07ad2b250dbd9b5afdce864135d4f0ec677
yuma140902 <yuma140902@gmail.com>
2019-03-10 08:26:38 +0900

    Update ModYumasSeasonsMod.java

--- src/main/java/yuma140902/yumas_seasons_mod/ModYumasSeasonsMod.java
+++ src/main/java/yuma140902/yumas_seasons_mod/ModYumasSeasonsMod.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
 import yuma140902.yumas_seasons_mod.util.Consts;
 
 @Mod(modid = ModYumasSeasonsMod.MOD_ID, version = ModYumasSeasonsMod.MOD_VERSION, useMetadata = true, guiFactory = Consts.MOD_CONFIG_GUI_FACTORY)
@@ -23,6 +24,8 @@ public class ModYumasSeasonsMod {
 	public static final String MOD_NAME = "Yuma's Seasons Mod";
 	public static final String MINECRAFT_VERSION = "1.7.10";
 	public static final String MOD_VERSION = "0.0.0";
+	public static final String MOD_TEXTURE_DOMAIN = "yumas_seasons_mod";
+	public static final CreativeTabs MOD_CREATIVETAB = new MyCreativeTab();
 	
 	private void loadModMetadata(ModMetadata modMetadata) {
 		modMetadata.modId = MOD_ID;
