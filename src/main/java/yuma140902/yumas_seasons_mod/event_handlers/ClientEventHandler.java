344bdf12ab105007565266284a004396ff39decc
yuma140902 <yuma140902@gmail.com>
2019-03-10 08:26:32 +0900

    鉱石辞書をツールチップに表示

--- src/main/java/yuma140902/yumas_seasons_mod/event_handlers/ClientEventHandler.java
+++ src/main/java/yuma140902/yumas_seasons_mod/event_handlers/ClientEventHandler.java
@@ -1,8 +1,29 @@
 package yuma140902.yumas_seasons_mod.event_handlers;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.oredict.OreDictionary;
+import yuma140902.yumas_seasons_mod.config.ModConfigCore;
+
 public class ClientEventHandler {
 private ClientEventHandler() {}
 	
 	public static final ClientEventHandler INSTANCE = new ClientEventHandler();
 	
+	@SubscribeEvent
+	public void onTooltipShown(ItemTooltipEvent event) {
+		if(ModConfigCore.debugMode) {
+			showOreDictionary(event);
+		}
+	}
+	
+	private static void showOreDictionary(ItemTooltipEvent event) {
+		ItemStack itemstack = event.itemStack;
+		int[] oreIDs = OreDictionary.getOreIDs(itemstack);
+		
+		for(int i = 0; i < oreIDs.length; ++i) {
+			event.toolTip.add(OreDictionary.getOreName(oreIDs[i]));
+		}
+	}
 }
