af8c45b01bb334d8427152f035a80056903d1a43
yuma140902 <yuma140902@gmail.com>
2019-03-10 08:59:48 +0900

    食料の回復度などをツールチップに表示するようにした

--- src/main/java/yuma140902/yumas_seasons_mod/event_handlers/ClientEventHandler.java
+++ src/main/java/yuma140902/yumas_seasons_mod/event_handlers/ClientEventHandler.java
@@ -1,6 +1,8 @@
 package yuma140902.yumas_seasons_mod.event_handlers;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.oredict.OreDictionary;
@@ -15,6 +17,7 @@ private ClientEventHandler() {}
 	public void onTooltipShown(ItemTooltipEvent event) {
 		if(ModConfigCore.debugMode) {
 			showOreDictionary(event);
+			showFoodHealAmount(event);
 		}
 	}
 	
@@ -24,6 +27,19 @@ private ClientEventHandler() {}
 		
 		for(int i = 0; i < oreIDs.length; ++i) {
 			event.toolTip.add(OreDictionary.getOreName(oreIDs[i]));
+		}
+	}
+	
+	private static void showFoodHealAmount(ItemTooltipEvent event) {
+		ItemStack itemstack = event.itemStack;
+		Item item = itemstack.getItem();
+		
+		if(item instanceof ItemFood) {
+			ItemFood food = (ItemFood)item;
+			int healAmount = food.func_150905_g(itemstack);
+			float saturationModifier = food.func_150906_h(itemstack);
+			
+			event.toolTip.add(healAmount + "(+* " + saturationModifier + ")");
 		}
 	}
 }
